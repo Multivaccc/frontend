af2fad58e00d7178353b7b88670cf00b6de9392e
ml5885 <44534695+ml5885@users.noreply.github.com>
2023-10-26 16:48:24 -0400

    Added loading message to Loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import "../styles/Loading.css";
 import { ThreeDots } from "react-loader-spinner";
 
-export default function Loading() {
+interface LoadingProps {
+	message?: string;
+}
+export default function Loading(data: LoadingProps) {
 	return (
 		<div className="loading-page">
-			<ThreeDots
-				height={80}
-				width={80}
-				radius={9}
-				color="black"
-				ariaLabel="three-dots-loading"
-				wrapperStyle={{}}
-				wrapperClass=""
-				visible={true}
-			/>
+			<div id="loading-icon">
+				<ThreeDots
+					height={80}
+					width={80}
+					radius={9}
+					color="black"
+					ariaLabel="three-dots-loading"
+					wrapperStyle={{}}
+					wrapperClass=""
+					visible={true}
+				/>
+			</div>
+			<p>{data.message ? data.message : "No loading message!"}</p>
 		</div>
 	);
 }
